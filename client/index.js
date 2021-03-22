91f0380b9989589f207b22501a00f424a796a724
Joao Marcos <joaomarcosnina@gmail.com>
2021-03-22 20:30:21 -0300

    #1 - Criando interfaces em programas de linha de comando com o pacote Blessed e o padrão Builder

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import Events from "events";
+import TerminalController from "./src/terminalController.js";
+
+const componentEmitter = new Events();
+
+const controller = new TerminalController();
+await controller.initializeTable(componentEmitter);
+
+
+
